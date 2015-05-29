4a8450adb400a295164d814591927e8c9e0a3983
Daniyar Itegulov <wibkwibk@gmail.com>
2015-05-29 22:19:51 +0300

    Fixed relative host issue

--- src/main/java/servlets/BoxAuthServlet.java
+++ src/main/java/servlets/BoxAuthServlet.java
@@ -18,7 +18,9 @@ public class BoxAuthServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.sendRedirect(String.format("https://app.box.com/api/oauth2/authorize?" +
-                "response_type=code&redirect_uri=http://localhost:5000/complete&client_id=%s&state=kudah",
+                "response_type=code&redirect_uri=http://%s:%d/complete&client_id=%s&state=kudah",
+                req.getServerName(),
+                req.getServerPort(),
                 BoxApiConstants.CLIENT_ID));
     }
 }
