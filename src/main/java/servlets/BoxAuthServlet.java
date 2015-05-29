9af30685452c138a5bf9be60df169e2f35d0fb5d
Daniyar Itegulov <wibkwibk@gmail.com>
2015-05-29 23:58:39 +0300

    Fixed http/https problem

--- src/main/java/servlets/BoxAuthServlet.java
+++ src/main/java/servlets/BoxAuthServlet.java
@@ -19,24 +19,8 @@ import java.io.PrintWriter;
 public class BoxAuthServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try (PrintWriter pw = resp.getWriter()) {
-            pw.println(req.getScheme());
-            pw.println(req.getServerName());
-            pw.println(req.getServerPort());
-            pw.println(String.format("https://app.box.com/api/oauth2/authorize?" +
-                            "response_type=code&redirect_uri=%s://%s:%d/complete&client_id=%s&state=kudah",
-                    req.getScheme(),
-                    req.getServerName(),
-                    req.getServerPort(),
-                    BoxApiConstants.CLIENT_ID));
-        }
-        /*
         resp.sendRedirect(String.format("https://app.box.com/api/oauth2/authorize?" +
-                "response_type=code&redirect_uri=%s://%s:%d/complete&client_id=%s&state=kudah",
-                req.getScheme(),
-                req.getServerName(),
-                req.getServerPort(),
+                        "response_type=code&redirect_uri=https://boxeliteapp.herokuapp.com/auth/complete&client_id=%s&state=kudah",
                 BoxApiConstants.CLIENT_ID));
-        */
     }
 }
