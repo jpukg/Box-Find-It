7dbf713901686af71376140267567d379020f37c
Daniyar Itegulov <wibkwibk@gmail.com>
2015-05-31 01:21:27 +0300

    Added default url for images

--- src/main/java/ourapp/BoxLister.java
+++ src/main/java/ourapp/BoxLister.java
@@ -9,10 +9,12 @@ import box.BoxFile;
  * @author Daniyar Itegulov
  */
 public class BoxLister {
+    private static final String URL = "https://pp.vk.me/c624218/v624218371/4764/D9C41NS_3vQ.jpg";
+
     public static void list(StringBuilder sb, BoxDirectory boxDirectory) {
         sb.append("{");
         sb.append("\"name\":\"").append(boxDirectory.getName()).append("\",");
-        sb.append("\"preview\":\"").append("sosi_hui").append("\",");
+        sb.append("\"preview\":\"").append(URL).append("\",");
         sb.append("\"subfiles\":[");
         for (int i = 0; i < boxDirectory.getElementList().size(); i++) {
             BoxElement element = boxDirectory.getElementList().get(i);
@@ -20,7 +22,7 @@ public class BoxLister {
                 BoxFile file = (BoxFile) element;
                 sb.append("{")
                         .append("\"name\":\"").append(file.getName()).append("\",")
-                        .append("\"preview\":\"").append("sosi_hui").append("\"")
+                        .append("\"preview\":\"").append(URL).append("\"")
                         .append("}");
             } else {
                 list(sb, (BoxDirectory) element);
