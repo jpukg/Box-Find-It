432b76c16cd8ab35273aca081f3643df2cfd7552
Daniyar Itegulov <wibkwibk@gmail.com>
2015-05-31 09:43:56 +0300

    Added more entity types

--- src/main/java/idolondemand/entityextractor/EntityType.java
+++ src/main/java/idolondemand/entityextractor/EntityType.java
@@ -4,7 +4,9 @@ package idolondemand.entityextractor;
  * @author Daniyar Itegulov
  */
 public enum EntityType {
-    PEOPLE_ENG("people_eng"), PLACES_ENG("places_eng"), COMPAINES_ENG("companies_eng");
+    PEOPLE_ENG("people_eng"), PLACES_ENG("places_eng"), COMPAINES_ENG("companies_eng"), ORGANIZATIONS("organizations"),
+    LANUGUAGES("laguages"), UNIVERSITY("universities"), FILMS("films"), ADDRESSES_US("address_us"), PERSONS("person_fullname_eng"),
+    NUMBER_PHONES("number_phone_us"), DATE("date_eng"), INTERNET("internet"), BANKACCOUNT("bankaccount_us");
 
     private String value;
 
